1e2d2c9ff1a6ddf6126e4bae31b962423ba77499
THLOgN <102423409+THLOgN@users.noreply.github.com>
2025-10-02 07:44:41 +0700

    init code

---
+++ booking/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
+import {Toaster } from "react-hot-toast";
+import Footer from './components/Footer';
+import SeatLayout from './pages/SeatLayout'
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+  return (
+
+  <>
+    <Toaster/>
+    {!isAdminRoute && <Navbar />}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+      <Route path="/movie/:id/:date" element={<SeatLayout />} />
+      <Route path="/my-bookings" element={<MyBookings />} />
+      <Route path="/favorite" element={<Favorite />} />
+    </Routes>
+        {!isAdminRoute && <Footer />}
+
+  </>)
+};
+
+export default App;

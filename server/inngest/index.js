4d29ceff1f1dc357bc54b2637e123b92270e2050
THLOgN <102423409+THLOgN@users.noreply.github.com>
2025-10-07 05:09:18 +0700

    `feat: Added Inngest function to release seats and delete booking after 10 minutes of payment check`

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,56 +1,82 @@
 import { Inngest } from "inngest";
-import User from "../models/User.js"
+import User from "../models/User.js";
 
 export const inngest = new Inngest({
   id: "movie-ticket-booking",
 });
 
-
-
 // Inngest Function to creat user data in database
 const syncUserCreation = inngest.createFunction(
-  {id: 'sync-user-from-clerk'},
-  { event: 'clerk/user.created' },
-  async ({ event })=>{
-    const {id, first_name, last_name, email_addresses, image_url} = event.data
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + ' ' + last_name,
-      image: image_url
-    }
-    await User.create(userData)
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.create(userData);
   }
-)
+);
 
 // Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-  {id: 'delete-user-with-clerk'},
-  { event: 'clerk/user.deleted' },
-  async ({ event })=>{
-    const {id} = event.data
-    await User.findByIdAndDelete(id)
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
   }
-)
+);
 
 // Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-  {id: 'update-user-from-clerk'},
-  { event: 'clerk/user.updated' },
-  async ({ event })=>{
-    const { id, first_name, last_name, email_addresses, image_url } = event.data
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + ' ' + last_name,
-      image: image_url
-    }
-    await User.findByIdAndUpdate(id, userData)
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
   }
-)
+);
+
+const releaseSeatsAndDeleteBooking = inngest.createFunction(
+  { id: "release-seats-delete-booking" },
+  { event: "app/checkpayment" },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
+
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
+
+      // If payment is not made, release seats and delete booking
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+        booking.bookedSeats.forEach((seat) => {
+          delete show.occupiedSeats[seat];
+        });
+        show.markModified("occupiedSeats");
+        await show.save();
+        await Booking.findByIdAndDelete(booking._id);
+      }
+    });
+  }
+);
 
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
+  releaseSeatsAndDeleteBooking,
 ];

23ac199aca9d9cd0639b08e1a299e1986769ae5a
Nguyen Thang Long <thanglong479@gmail.com>
2026-05-04 20:33:40 +0700

    fix: remove duplicate import statement for ClerkProvider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { AppProvider } from "./context/AppContext.jsx";
-import { ClerkProvider } from '@clerk/react';
+
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 

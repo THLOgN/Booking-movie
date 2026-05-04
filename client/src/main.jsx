b7a0952dca71c05e0e73c0d3e962729af718e113
Nguyen Thang Long <thanglong479@gmail.com>
2026-05-04 20:31:46 +0700

    fix: correct import statement for ClerkProvider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
+import { ClerkProvider } from "@clerk/clerk-react";
 import { AppProvider } from "./context/AppContext.jsx";
 import { ClerkProvider } from '@clerk/react';
 // Import your Publishable Key
